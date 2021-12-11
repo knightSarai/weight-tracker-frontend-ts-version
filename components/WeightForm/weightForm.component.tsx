506f1029a9dcb2ceaa6eb59621e50ecf22ba189a
knight <knight.sarai.dev@gmail.com>
2021-12-11 23:40:32 +0200

    Add WeightForm component Add width to WeightInput

--- components/WeightForm/weightForm.component.tsx
+++ components/WeightForm/weightForm.component.tsx
@@ -1,16 +1,43 @@
+import { useState } from 'react';
+import DateTimePicker from '@mui/lab/DateTimePicker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SaveIcon from '@mui/icons-material/Save';
 import WeightInput from '@/components/WeightInput';
 
 export default function WeightForm() {
+  const [loading, setLoading] = useState(false);
+  function handleClick() {
+    setLoading(true);
+  }
+  const [value, setValue] = useState<Date | null>(new Date());
+  const handleChange = (newValue: Date | null) => {
+    setValue(newValue);
+  };
   return (
-    <Box
-      component="form"
-      mt={5}
-      display="flex"
-      justifyContent="center"
-      autoComplete="off"
-    >
+    <Box display="flex" justifyContent="space-between" width="60%">
       <WeightInput />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DateTimePicker
+          label="Date&Time picker"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      <LoadingButton
+        color="primary"
+        onClick={handleClick}
+        loading={loading}
+        loadingPosition="start"
+        startIcon={<SaveIcon />}
+        variant="contained"
+      >
+        Save
+      </LoadingButton>
     </Box>
   );
 }
--- components/WeightInput/weightInput.component.tsx
+++ components/WeightInput/weightInput.component.tsx
@@ -1,5 +1,11 @@
 import { CssTextField } from './styles';
 
 export default function WeightInput() {
-  return <CssTextField label="Weight Input" id="weight-input" />;
+  return (
+    <CssTextField
+      sx={{ width: '140px' }}
+      label="Weight Input"
+      id="weight-input"
+    />
+  );
 }
